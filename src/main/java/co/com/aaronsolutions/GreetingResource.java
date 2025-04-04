b9e26a60aca51dc949bd96dd0a43e3fa89c06e27
Raul Londono Murillo <raullondonomurillo@gmail.com>
2025-04-04 10:12:37 -0500

    Update project name

--- src/main/java/co/com/aaronsolutions/GreetingResource.java
+++ src/main/java/co/com/aaronsolutions/GreetingResource.java
@@ -1,4 +1,4 @@
-package org.acme;
+package co.com.aaronsolutions;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
--- src/test/java/co/com/aaronsolutions/GreetingResourceIT.java
+++ src/test/java/co/com/aaronsolutions/GreetingResourceIT.java
@@ -1,4 +1,4 @@
-package org.acme;
+package co.com.aaronsolutions;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
